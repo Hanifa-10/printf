5e997974dcaec0f226bd3e504da56bef1350f99b
Hanifa-10 <chep.hanifa@gmail.com>
2021-07-28 06:43:06 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,83 @@
+#include "holberton.h"
+
+/**
+* get_fmt_func - database of functions
+* @s: string being passed
+* Return: char to be printed
+*/
+
+int (*get_fmt_func(cons char s))(va_list)
+{
+int i = 0;
+fmt format[] = {
+{"c", print_char},
+{"s", print_str},
+{"%", print_per},
+{"d", print_int},
+{"i", print_int},
+{"b", print_bin},
+{"u", print_unsigned},
+{"o", print_oct},
+{"x", print_hex},
+{"X", print_Hex},
+{"R", rot13},
+{NULL, NULL},
+};
+
+while (i < 11)
+{
+if (*(format[i].type) == s )
+{
+return (format[i].f);
+}
+i++;
+}
+return (0);
+}
+return (0);
+}
+
+/**
+ * _printf - produces output according to format
+ * @format: character string
+ * Return: the number of characters printed 
+ */
+
+int _printf(const char *format, ...)
+{
+va_list args;
+int c, i;
+int (*ptr)(va_list);
+
+c = 0;
+i = 0;
+
+if (format = NULL)
+return (-1);
+
+va_start(args, format);
+while (format[i] != 0)
+if (format[i] == 37)
+{
+i++;
+ptr = get_fmt_func(format[i]);
+if (!ptr)
+{
+if (format[i] == 0)
+return (-1);
+
+c += _putchar(format[i - 1]);
+c += _putchar(format[i]);
+}
+else
+c += ptr(args);
+}
+else
+c += _putchar(format[i]);
+
+i++;
+}
+va_end(args);
+return (c);
+}
+
---
+++ holberton.h
@@ -0,0 +1,36 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+* struct fmt - function to check for formats
+* @type: the format to print
+* @f: the print function to use
+*/
+typedef struct format
+{
+char *type;
+int (*f)(va_list);
+} fmt;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_per(va_list args);
+int print_int(va_list args);
+int print_bin(va_list args);
+int print_unsigned(va_list args);
+char *convert(long int num, int base);
+int print_oct(va_list args);
+int print_hex(va_list args);
+int print_Hex(va_list args);
+int _pow(int x, int y);
+int sqrt(int n);
+int rot13(va_list args);
+
+
+#endif
