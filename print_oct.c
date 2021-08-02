611636876fb5a15cb19b53a09c78c385e58d9622
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-08-02 09:49:31 +0300

    Update print_oct.c

---
+++ print_oct.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* print_oct - function to print octal
+* @args: list being passed
+* Return: updated count
+*/
+int print_oct(va_list args)
+{
+int i;
+char *str;
+
+str = convert(va_arg(args, int), 8);
+
+if (str == NULL)
+str = "NULL";
+
+i = 0;
+
+while (str[i] != 0)
+i += _putchar(str[i]);
+
+return (i);
+}
